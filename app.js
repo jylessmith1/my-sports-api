cc8e836d3707e879ad459a080e9b74cb6231a563
Rodney Edge Jr <rodneyedgejr@gmail.com>
2023-04-14 15:16:16 -0400

    added cors to your api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/customers');
@@ -10,6 +11,8 @@ const storeRouter = require('./routes/store')
 
 const app = express();
 
+// now using cors
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
